6bd88aad79f9d1fb3f74c766b557c488e2b15ad5
Thiago Medeiros <si-thiago@hotmail.com>
2019-07-11 22:59:30 -0300

    small changes to test gh-pages deployment

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 			<h4>Javascript stack used</h4>
 
 			<ul id="navlist" style={styles.list}>
-				<li><a href="https://github.com/facebook/react">react</a></li>
+				<li><a href="https://github.com/facebook/react">react.js</a></li>
 				<li><a href="https://github.com/LucasBassetti/react-simple-chatbot">simple-chat-bot</a></li>
 				<li><a href="https://github.com/moment/moment">moment</a></li>
 				<li><a href="https://github.com/axios/axios">axios</a></li>
