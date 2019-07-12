9efc925929fbf16b048d521b30cf63b60aafe3f6
Thiago Medeiros <si-thiago@hotmail.com>
2019-07-11 23:58:58 -0300

    Updates

---
+++ precache-manifest.07a168316946d80df2b97b8708dcaedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf44147551096108fbca413030e8a9f",
+    "url": "/goofy-chatbot/index.html"
+  },
+  {
+    "revision": "68aba96465dd5220f907",
+    "url": "/goofy-chatbot/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9670e8a0830dcf9fac6e",
+    "url": "/goofy-chatbot/static/js/2.81fe1e2a.chunk.js"
+  },
+  {
+    "revision": "68aba96465dd5220f907",
+    "url": "/goofy-chatbot/static/js/main.53c8fbd4.chunk.js"
+  },
+  {
+    "revision": "7e5db1432dbd7224e04d",
+    "url": "/goofy-chatbot/static/js/runtime~main.50e8889e.js"
+  }
+]);
